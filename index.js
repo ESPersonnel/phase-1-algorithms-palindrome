93ab572d655ad17218a7147b0159ea10a4832af7
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-06-16 10:51:26 +0300

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  let right = word.length - 1;
+  let left = 0;
+  while (left < right) { // while left is less than right
+    if (word[left] !== word[right]) { // if left is not equal to right
+      return false; // return false
+    }
+    left++; // increment left
+    right--; // decrement right
+  }
+  return true; // return true
 }
 
 /* 
   Add your pseudocode here
+  Input: string
+  Output: boolean
+  Steps to solve the problem:
+  1. reverse the string
+  2. compare the reversed string to the original string
+  3. return true if they are the same
+  4. return false if they are not the same
 */
 
 /*
   Add written explanation of your solution here
+  You need to reverse the string and compare it to the original string.
+  The first step is to reverse the string.
+  The second step is to compare the reversed string to the original string.
+  The third step is to return true if they are the same and false if they are not.
 */
 
 // You can run `node index.js` to view these console logs
